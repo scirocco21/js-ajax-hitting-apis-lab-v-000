ccdd05b69276d0f6344123785cad2c3b65890885
Seb Gertz <sebgertz@gmail.com>
2018-08-22 16:48:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ function getCommits(el) {
 }
 
 
-function showCommits() {
+function displayCommits() {
   const commits = JSON.parse(this.responseText)
   const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
   document.getElementById("details").innerHTML += commitsList
