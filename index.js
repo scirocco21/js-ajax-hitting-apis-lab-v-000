3a17b3f9b256fd6dfc998014d3290e6c9606df23
Seb Gertz <sebgertz@gmail.com>
2018-08-22 15:59:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ function showRepositories(event, data) {
   var repos = JSON.parse(this.responseText)
   const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`;
 
-
   document.getElementById("repositories").innerHTML = repoList
 }
 
